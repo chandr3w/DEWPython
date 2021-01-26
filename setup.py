5b966dc4dcea9910aa722158b573816c1b94a421
Andrew Chan <70087509+chandr3w@users.noreply.github.com>
2021-01-26 10:08:42 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='DEWPython',
-    version='1.2.0',
+    version='1.3.0',
     author='chandr3w',
     author_email='amchan@caltech.edu',
     description='Python-Implemented Deep Earth Water Model',
